6207d6b0ca24d407d4e0864b18a79a498a254c2c
Diego Martinez <diegomadeit@gmail.com>
2019-04-03 16:50:18 +0200

    First test with Jest and Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "./App";
+import Header from "./components/header/Header";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it("should have Header", () => {
+    wrapper = shallow(<App />);
+    expect(wrapper.contains(<Header />)).toBe(true);
+  });
+
+  it("should have a main section", () => {
+    wrapper = shallow(<App />);
+    expect(wrapper.find("section").length).toBe(1);
+  });
 });

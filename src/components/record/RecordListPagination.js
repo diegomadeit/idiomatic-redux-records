6692b0b045d88bc9f63b4b78a184ad9c00893e8a
Diego Martinez <diegomadeit@gmail.com>
2019-04-05 15:17:09 +0200

    Fix issue after adding tests

--- src/components/record/RecordListPagination.js
+++ src/components/record/RecordListPagination.js
@@ -3,36 +3,34 @@ import PropTypes from "prop-types";
 import styles from "./RecordListPagination.module.css";
 import HoverLink from "../../components/common/HoverLink";
 
-const RecordListPagination = ({ pagination, collectionPath }) =>
-  pagination.page &&
-  pagination.pages && (
-    <div className={styles.pagination}>
-      {pagination.page !== 1 && (
-        <HoverLink
-          to={{
-            pathname: `${collectionPath}`,
-            search: `?page=${pagination.page - 1}`
-          }}
-          className="button"
-          hoverClass="button--hover"
-        >
-          <span>Previous</span>
-        </HoverLink>
-      )}
-      {pagination.page !== pagination.pages && (
-        <HoverLink
-          to={{
-            pathname: `${collectionPath}`,
-            search: `?page=${pagination.page + 1}`
-          }}
-          className="button"
-          hoverClass="button--hover"
-        >
-          <span>Next</span>
-        </HoverLink>
-      )}
-    </div>
-  );
+const RecordListPagination = ({ pagination, collectionPath }) => (
+  <div className={styles.pagination}>
+    {pagination.page !== 1 && (
+      <HoverLink
+        to={{
+          pathname: `${collectionPath}`,
+          search: `?page=${pagination.page - 1}`
+        }}
+        className="button"
+        hoverClass="button--hover"
+      >
+        <span>Previous</span>
+      </HoverLink>
+    )}
+    {pagination.page !== pagination.pages && (
+      <HoverLink
+        to={{
+          pathname: `${collectionPath}`,
+          search: `?page=${pagination.page + 1}`
+        }}
+        className="button"
+        hoverClass="button--hover"
+      >
+        <span>Next</span>
+      </HoverLink>
+    )}
+  </div>
+);
 
 RecordListPagination.propTypes = {
   pagination: PropTypes.object.isRequired,

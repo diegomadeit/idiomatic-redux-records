f4dba9571b4d22a84be22cd85e70c690d183f209
Diego Martinez <diegomadeit@gmail.com>
2019-04-08 20:17:55 +0200

    Test action creators and async action creators

---
+++ src/actions/actions.test.js
@@ -0,0 +1,78 @@
+import * as actions from ".";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
+
+describe("actions", () => {
+  describe("action creators", () => {
+    it("should create searchArtist action", () => {
+      const artist = "Pink Floyd";
+      const expectedAction = {
+        type: actions.ActionTypes.SEARCH_ARTIST,
+        artist
+      };
+      expect(actions.searchArtist(artist)).toEqual(expectedAction);
+    });
+  });
+
+  describe("async actions", () => {
+    const middlewares = [thunk];
+    const mockStore = configureMockStore(middlewares);
+    let store;
+
+    afterEach(() => {
+      fetchMock.restore();
+    });
+
+    it("should dispatch FETCH_COLLECTION_SUCCESS when fetching collection", async () => {
+      fetchMock.getOnce("end:/releases", {
+        body: { pagination: {}, releases: [] },
+        headers: { "content-type": "application/json" }
+      });
+
+      const expectedActions = [
+        {
+          type: actions.ActionTypes.FETCH_COLLECTION_REQUEST
+        },
+        {
+          type: actions.ActionTypes.FETCH_COLLECTION_SUCCESS,
+          pagination: {},
+          releases: []
+        }
+      ];
+
+      store = mockStore({ collection: {} });
+
+      await store.dispatch(actions.fetchCollection());
+
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+
+    it("should dispatch FETCH_COLLECTION_ITEM_SUCCESS when fetching collection item", async () => {
+      fetchMock.get(/(releases|artists)\/\d+/, {
+        body: {},
+        headers: { "content-type": "application/json" }
+      });
+
+      const expectedActions = [
+        {
+          type: actions.ActionTypes.FETCH_COLLECTION_ITEM_REQUEST
+        },
+        {
+          type: actions.ActionTypes.FETCH_COLLECTION_ITEM_SUCCESS,
+          artist: {},
+          release: {}
+        }
+      ];
+
+      store = mockStore({ collectionItem: {} });
+
+      const params = { artistId: "8699", recordId: "3208548" };
+      await store.dispatch(
+        actions.fetchCollectionItem(params.artistId, params.recordId)
+      );
+
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
--- src/actions/index.js
+++ src/actions/index.js
@@ -15,49 +15,46 @@ export const searchArtist = artist => ({
   artist
 });
 
-export const fetchCollection = opts => (dispatch, getState) => {
+export const fetchCollection = opts => async dispatch => {
   dispatch({
     type: ActionTypes.FETCH_COLLECTION_REQUEST
   });
 
-  return api.fetchRecords(opts).then(
-    collection => {
-      dispatch({
-        type: ActionTypes.FETCH_COLLECTION_SUCCESS,
-        pagination: collection.pagination,
-        releases: collection.releases
-      });
-    },
-    error => {
-      dispatch({
-        type: ActionTypes.FETCH_COLLECTION_FAILURE,
-        message: error.message || "Something went wrong."
-      });
-    }
-  );
+  try {
+    const collection = await api.fetchRecords(opts);
+    dispatch({
+      type: ActionTypes.FETCH_COLLECTION_SUCCESS,
+      pagination: collection.pagination,
+      releases: collection.releases
+    });
+  } catch (error) {
+    dispatch({
+      type: ActionTypes.FETCH_COLLECTION_FAILURE,
+      message: error.message || "Something went wrong."
+    });
+  }
 };
 
-export const fetchCollectionItem = (artistId, recordId) => (
-  dispatch,
-  getState
-) => {
+export const fetchCollectionItem = (artistId, recordId) => async dispatch => {
   dispatch({
     type: ActionTypes.FETCH_COLLECTION_ITEM_REQUEST
   });
 
-  Promise.all([api.fetchArtist(artistId), api.fetchRecord(recordId)]).then(
-    ([artist, release]) => {
-      dispatch({
-        type: ActionTypes.FETCH_COLLECTION_ITEM_SUCCESS,
-        artist,
-        release
-      });
-    },
-    error => {
-      dispatch({
-        type: ActionTypes.FETCH_COLLECTION_ITEM_FAILURE,
-        message: error.message || "Something went wrong."
-      });
-    }
-  );
+  try {
+    const [artist, release] = await Promise.all([
+      api.fetchArtist(artistId),
+      api.fetchRecord(recordId)
+    ]);
+
+    dispatch({
+      type: ActionTypes.FETCH_COLLECTION_ITEM_SUCCESS,
+      artist,
+      release
+    });
+  } catch (error) {
+    dispatch({
+      type: ActionTypes.FETCH_COLLECTION_ITEM_FAILURE,
+      message: error.message || "Something went wrong."
+    });
+  }
 };

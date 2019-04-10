1c4981380f1c0f9c049592dcb211a1fb47dea077
Diego Martinez <diegomadeit@gmail.com>
2019-04-10 14:56:49 +0200

    Test visitedReleasesSelector Memoized Selector

--- src/reducers/visitedReleases.test.js
+++ src/reducers/visitedReleases.test.js
@@ -1,5 +1,6 @@
 import visitedReleases from "./visitedReleases";
 import { ActionTypes } from "../actions";
+import { visitedReleasesSelector } from ".";
 
 describe("visitedReleases reducer", () => {
   it("should return the initial state", () => {
@@ -27,5 +28,32 @@ describe("visitedReleases reducer", () => {
         release: { id: 1 }
       })
     ).toEqual([{ id: 1 }, { id: 2 }]);
+  });
+
+  it("should visitedReleasesSelector memoization work correctly", () => {
+    const withMockState = reducer => (...args) => ({
+      visitedReleases: reducer(...args)
+    });
+
+    const getMockState = withMockState(visitedReleases);
+
+    let state = getMockState([], {
+      type: ActionTypes.FETCH_COLLECTION_ITEM_SUCCESS,
+      release: { id: 1 }
+    });
+    expect(visitedReleasesSelector(state)).toEqual([{ id: 1 }]);
+
+    state = getMockState(state.visitedReleases, {
+      type: ActionTypes.FETCH_COLLECTION_SUCCESS
+    });
+    expect(visitedReleasesSelector(state)).toEqual([{ id: 1 }]);
+    expect(visitedReleasesSelector.recomputations()).toBe(1);
+
+    state = getMockState(state.visitedReleases, {
+      type: ActionTypes.FETCH_COLLECTION_ITEM_SUCCESS,
+      release: { id: 2 }
+    });
+    expect(visitedReleasesSelector(state)).toEqual([{ id: 2 }, { id: 1 }]);
+    expect(visitedReleasesSelector.recomputations()).toBe(2);
   });
 });
